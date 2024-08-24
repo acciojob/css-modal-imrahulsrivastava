9a36afe968b1b46567e0a5f9d5ef39c417d0c051
Rahul Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-08-24 10:29:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function toggleModal() {
+  const modal = document.querySelector(".modal");
+  if (isModalOpen) {
+    modal.style.display = "none";
+    isModalOpen = false;
+  } else {
+    modal.style.display = "flex";
+    isModalOpen = true;
+  }
+}
+
+let isModalOpen = false;
+
+const openButton = document.getElementById("openModal");
+openButton.addEventListener("click", toggleModal);
+
+const closeButton = document.querySelector(".close-modal");
+closeButton.addEventListener("click", toggleModal);
